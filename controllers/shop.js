eb310f988674aae0fd0353084f61ce2cff6267d0
Suuyshi <sarah.elsherbinyy@gmail.com>
2022-11-12 16:07:50 +0200

    added delete from cart feature

--- controllers/shop.js
+++ controllers/shop.js
@@ -39,7 +39,24 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", { path: "/cart", pageTitle: "Your Cart" });
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products) {
+        const cartProductData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductData) {
+          cartProducts.push({ productData: product, qty: cartProductData.qty });
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
+    });
+  });
 };
 
 exports.postCart = (req, res, next) => {
@@ -48,6 +65,15 @@ exports.postCart = (req, res, next) => {
     Cart.addProduct(prodId, product.price);
     res.redirect("/cart");
   });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.getProduct(prodId, (product) => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  });
+  Cart.deleteProduct();
 };
 
 exports.getCheckout = (req, res, next) => {
